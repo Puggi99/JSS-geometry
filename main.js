557cd8e883ce93d8578b3fc388df811f60ec9130
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-04-03 13:31:59 +0200

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+const point1 = new Point(1,1);
+const point2 = new Point(4,5);
+console.log(point1)
+console.log(point2)
+// ________________________________________________
+
+const segment1 = new Segment(point1,point2)
+console.log(segment1)
+
+const segment2 = Segment.fromPointsCoordinates(0,1,3,4)
+console.log(segment2)
